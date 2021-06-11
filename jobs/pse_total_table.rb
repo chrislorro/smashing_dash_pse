1e0a2e59a84cdeb7a7a7ea59c01ce9a450ef9907
chris lawrence <chris.lawrence@puuppet.com>
2021-06-11 09:55:58 +0100

    working  dashboard, working on clock

---
+++ jobs/pse_total_table.rb
@@ -0,0 +1,44 @@
+require 'puppet_forge'
+require 'pry'
+
+PuppetForge.user_agent = "PseForgeData" # parameter is required when making API calls on the forge
+
+
+def find_user(username)
+    usrid   = username
+    user    = PuppetForge::User.find(username) # The Puppetforge::User object retrieves all data from the forge API
+    count = user.module_count             # The module_count method returns the total number of modules for the user
+    release = user.release_count            # The release_count returns the total number of releases for a user
+
+    modules = PuppetForge::Module.where(owner: username) 
+
+    # Add all the numbers together using reduce
+    totals = modules.unpaginated.reduce(0) do | sum, mod |                   
+        sum + mod.downloads
+    end
+
+    return { cols: [ {value: usrid}, {value: totals}, {value: count}, {value: release} ]}, totals
+
+end
+
+forge_a = [ "puppetlabs", "dylanratcliffe", "jesse", "benjaminrobertson" ]  # All user names hat have forge modules should be added to this array
+
+SCHEDULER.every '1s' do
+
+    hrows = [
+        { cols: [ {value: 'Contributor'}, {value: 'Downloads'}, {value: 'User mods'}, {value: 'User release'} ] }
+    ]
+
+    rows = []
+    all_totals = []
+
+    forge_a.each do | username |
+        row = find_user(username)             
+        rows << row
+    end
+
+
+    send_event('team-table', { hrows: hrows, rows: rows })
+    #send_event('all_download', { current: all_totals })
+
+end
